cffb2121e9473dec75d6a77629eeecc1778e9e8d
berrouayel djamila <102658745+berrouayeldjamila@users.noreply.github.com>
2022-04-07 17:12:40 +0200

    Update ClientColyseus.js

--- src/Component/PageMultiplayer/ClientColyseus.js
+++ src/Component/PageMultiplayer/ClientColyseus.js
@@ -7,7 +7,7 @@ class ClientColyseus extends React.Component {
 
   constructor(props) {
     super(props);
-  this.client = new Colyseus.Client('ws://' + "syntaxmapfrontmain.herokuapp.com");
+  this.client = new Colyseus.Client('ws://' + "localhost:" + "8080");
     this.room = null;
       
     this.state = {
