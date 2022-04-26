60a51023b3fac06314c6dc6f5685efea3a2ba54e
berrouayel djamila <102658745+berrouayeldjamila@users.noreply.github.com>
2022-04-26 13:26:13 +0100

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -105,7 +105,7 @@ exports.config = {
     baseUrl: url[process.env.ENV],
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout:120000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
@@ -149,7 +149,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 120000
     },
     //
     // =====
@@ -158,7 +158,7 @@ exports.config = {
     // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
     // it and to build services around it. You can either apply a single function or an array of
     // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
-    // resolved to continue.
+    // resolved to continue. Lol
     /**
      * Gets executed once before all workers get launched.
      * @param {Object} config wdio configuration object
